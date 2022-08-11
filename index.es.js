3a794b91ce7d92bab1d50a4960e4f88fc2542bae
Seanghay Yath <seanghay.dev@gmail.com>
2022-08-11 19:00:05 +0700

    feat: add esm

---
+++ index.es.js
@@ -0,0 +1,64 @@
+export function addMinutes(time, minute) {
+  const [h, m] = decode(time);
+  return encode(h, m + minute)
+}
+
+export function addHours(time, hour) {
+  const [h, m] = decode(time);
+  return encode(h + hour, m)
+}
+
+export function encode(hour = 0, minute = 0) {
+
+  if (minute >= 60) {
+    hour += Math.floor(minute / 60);
+    minute = minute % 60
+  }
+
+  if (hour >= 24) hour = hour % 24
+  return (hour * 100) + minute
+}
+
+export function decode(time) {
+  let hour = Math.floor(time / 100)
+  let minute = (time % 100);
+
+  if (minute >= 60) {
+    hour += Math.floor(minute / 60);
+    minute = minute % 60
+  }
+
+  if (hour >= 24) hour = hour % 24
+  return [hour, minute]
+}
+
+export function normalize(time) {
+  let hour = Math.floor(time / 100)
+  let minute = (time % 100);
+
+  if (minute >= 60) {
+    hour += Math.floor(minute / 60);
+    minute = minute % 60
+  }
+
+  if (hour >= 24) hour = hour % 24
+  return (hour * 100) + minute
+}
+
+export function isBetween(value, timeLeft, timeRight) {
+
+  let left;
+  let right;
+  
+  value = normalize(value);
+
+  if (timeLeft > timeRight) {
+    left = normalize(timeRight);
+    right = normalize(timeLeft);
+  } else {
+    left = normalize(timeLeft);
+    right = normalize(timeRight);
+  }
+
+  return value >= left && value <= right;
+}
